df4900393e052b81da30fa3929ca7db753b9cea5
cmchen <2425698138@qq.com>
2026-02-19 14:15:24 +0000

    feat(about): merge traits into about section and add dual activity heatmap

--- src/data/about-profile.ts
+++ src/data/about-profile.ts
@@ -1,13 +1,17 @@
 export type AboutProfile = {
   mbti: string
+  mbtiLink: string
   major: string
+  majorLink: string
   recentDoing: string
   recentReading: string
 }
 
 const aboutProfile: AboutProfile = {
   "mbti": "INTJ",
+  "mbtiLink": "https://www.16personalities.com/ch/intj-%E4%BA%BA%E6%A0%BC",
   "major": "电子信息工程",
+  "majorLink": "https://baike.baidu.com/item/%E7%94%B5%E5%AD%90%E4%BF%A1%E6%81%AF%E5%B7%A5%E7%A8%8B/667710",
   "recentDoing": "持续优化博客后台与前端交互体验",
   "recentReading": "局外人"
 }
