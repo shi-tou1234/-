b5cd51a9ed0792e7cd36c5bdb18309818daba81a
cmchen <2425698138@qq.com>
2026-02-21 16:06:35 +0800

    about: update profile

--- src/data/about-profile.ts
+++ src/data/about-profile.ts
@@ -12,7 +12,7 @@ const aboutProfile: AboutProfile = {
   "mbtiLink": "https://www.16personalities.com/ch/intj-%E4%BA%BA%E6%A0%BC",
   "major": "电子信息工程",
   "majorLink": "https://baike.baidu.com/item/%E7%94%B5%E5%AD%90%E4%BF%A1%E6%81%AF%E5%B7%A5%E7%A8%8B/87638",
-  "recentDoing": "持续优化博客后台与前端交互体验",
+  "recentDoing": "画电路图，偶尔更新博客",
   "recentReading": "局外人"
 }
 
