e7afef038128966060fbf4ba30ba56fe5f38193b
cmchen <2425698138@qq.com>
2026-03-08 18:41:21 +0800

    about: update profile

--- src/data/about-profile.ts
+++ src/data/about-profile.ts
@@ -13,7 +13,7 @@ const aboutProfile: AboutProfile = {
   "major": "电子信息工程",
   "majorLink": "https://baike.baidu.com/item/%E7%94%B5%E5%AD%90%E4%BF%A1%E6%81%AF%E5%B7%A5%E7%A8%8B/87638",
   "recentDoing": "大一下学期开始啦，努力学习中，随缘更新",
-  "recentReading": "局外人"
+  "recentReading": "投资与理财"
 }
 
 export default aboutProfile
